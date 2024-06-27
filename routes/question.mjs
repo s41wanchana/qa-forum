22dc11f803568eb249d41d9460e5faa0ec3da2bc
Wanchana <s.wanchana41@gmail.com>
2024-06-27 13:11:53 +0700

    feat: add post answer api and get answer api, optional requirement no.2 & 3

--- routes/question.mjs
+++ routes/question.mjs
@@ -34,6 +34,43 @@ questionRouter.post("/", async (req, res) => {
   });
 });
 
+questionRouter.post("/:id/answers", async (req, res) => {
+  const questionId = req.params.id;
+  const newAnswer = {
+    ...req.body,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  if (newAnswer.content.length > 300) {
+    return res.status(400).json({
+      message: "Content exceeds 300 characters",
+    });
+  }
+
+  try {
+    await connectionPool.query(
+      `insert into answers (question_id, content, created_at, updated_at)
+        values ($1, $2, $3, $4)`,
+      [
+        questionId,
+        newAnswer.content,
+        newAnswer.created_at,
+        newAnswer.updated_at,
+      ]
+    );
+  } catch (error) {
+    return res.status(400).json({
+      message: "Missing or invalid request data",
+      error: error.message,
+    });
+  }
+
+  return res.status(201).json({
+    message: "Answer created successfully.",
+  });
+});
+
 questionRouter.get("/", async (req, res) => {
   let result;
   const title = req.query.title;
@@ -85,6 +122,34 @@ questionRouter.get("/:id", async (req, res) => {
 
   return res.status(200).json({
     message: "Successfully retrieved the questions.",
+    data: result.rows,
+  });
+});
+
+questionRouter.get("/:id/answers", async (req, res) => {
+  let result;
+  const questionId = req.params.id;
+
+  try {
+    result = await connectionPool.query(
+      `select * from answers where question_id=$1`,
+      [questionId]
+    );
+  } catch (error) {
+    return res.status(400).json({
+      message: "Could not find server",
+      error: error.message,
+    });
+  }
+
+  if (result.rowCount === 0) {
+    return res.status(404).json({
+      message: "Question not found",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Successfully retrieved the answer.",
     data: result.rows,
   });
 });
